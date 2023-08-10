eb04d60caac1733f92cd77c77df1079d0eb5de7b
Aditya <adityamaurya5225@gmail.com>
2023-08-10 14:17:55 +0530

    Initial commit

--- services/transactions/routes/transactionsRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const transactionController = require('../controllers/transactionController');
-
-router.get('/fetch_transactions', transactionController.fetchtransactions);
-router.post('/add_transaction', transactionController.addTransaction);
-
-module.exports = router;
--- services/users/routes/usersRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const usersController = require('../controllers/usersController');
-
-router.get('/fetch_users', usersController.fetchUsers);
-router.post('/add_users', usersController.addUsers);
-
-module.exports = router;
