d93da730de2629178197191c9fdea57cc65bc95a
Aditya <adityamaurya5225@gmail.com>
2023-08-28 16:27:58 +0530

    redis

--- services/transactions/routes/transactionsRoute.js
+++ services/transactions/routes/transactionsRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const transactionController = require('../controllers/transactionController');
 const redis = require('redis');
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({ url: process.env.REDIS_PORT });
 
 redisClient.connect();
 redisClient.on('ready', () => console.log("Redis Connected!"));
